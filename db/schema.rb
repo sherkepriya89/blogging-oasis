684e866f4d735d6f63d8bb290955ffdd5ee04039
Priya Sherke <sherkepriya89@gmail.com>
2023-06-30 15:35:02 -0400

    added migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_192847) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
