982717aee55394e1afc56d896bf42b61f5548191
Priya Sherke <sherkepriya89@gmail.com>
2023-06-30 14:02:30 -0400

    Fixed project errors

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem "rails", "~> 7.0.6"
 gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
@@ -49,6 +49,7 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
+gem "sqlite3", "~> 1.4"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
@@ -70,3 +71,11 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+group :production do
+  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
+  gem "pg"
+end
+
+gem "erb-formatter"
+gem "bootstrap", "~> 5.2.3"
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+
+def show
+    
+end
+def index
+    @articles = Article.all
+end
+
+def new
+  @article = Article.new
+end
+
+def edit
+  
+end 
+
+def create
+    @article = Article.new(articles_params)
+   if @article.save
+    flash[:notice] = "Article was created successfully."
+    redirect_to @article
+   else
+    render 'new'
+   end
+end
+
+def update
+    if @article.update(articles_params)
+    flash[:notice] = "Article was updated successfully."
+    redirect_to @article
+   else
+    render 'edit'
+   end
+end
+
+def destroy
+    @article.destroy
+    redirect_to articles_path
+end 
+
+private
+def set_article
+    @article = Article.find(params[:id])
+end
+
+def articles_params
+    params.require(:article).permit(:title, :description)
+end
+
+end
