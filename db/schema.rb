eed93a2dcf54ad39f1c373602d777609d57c9c17
Priya Sherke <sherkepriya89@gmail.com>
2023-06-30 15:08:57 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 0) do
-    create_table "articles", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
