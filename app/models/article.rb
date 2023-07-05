37a8368683dec3dd6832462a505ca6e03af7cb43
Priya Sherke <sherkepriya89@gmail.com>
2023-07-05 09:29:33 -0400

    Styling of Create and Edit Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,3 +2,4 @@ class Article < ApplicationRecord
 validates :title, presence: true, length:{minimum: 6, maximum:100}
 validates :description, presence: true, length:{minimum: 10, maximum:1000}
 end
+
