dff9df3b5f36c105d48b652fa8082278a17ecd09
Priya Sherke <sherkepriya89@gmail.com>
2024-01-23 14:47:12 -0500

    modified navigation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+$(document).ready(function () { $('.ui.dropdown').dropdown(); })
