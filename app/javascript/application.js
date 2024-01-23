62b45b2dba3b00ef6613355f75a88485b17b12a2
Priya Sherke <sherkepriya89@gmail.com>
2024-01-23 15:04:27 -0500

    modified for dropdown error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require jquery
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require semantic-ui
-//= require_tree .
-
-$(document).ready(function () { $('.ui.dropdown').dropdown(); })
+require("jquery");
+import "bootstrap";
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
