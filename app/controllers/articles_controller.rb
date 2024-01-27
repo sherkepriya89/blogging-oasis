a53bb833bff0eef7b015f8d2d0a6b62c1508aaf5
Priya Sherke <sherkepriya89@gmail.com>
2024-01-27 17:55:56 -0500

    Added many to many associations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,7 +49,7 @@ def set_article
 end
 
 def articles_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
 end
 
 def require_same_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_24_213419) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_27_223337) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
